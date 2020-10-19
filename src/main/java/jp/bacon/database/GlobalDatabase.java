fd52817e1ac6542defd1b78b0ad1f4fa980aef80
watabe-hiroaki <watabe_hiroaki@cyberagent.co.jp>
2020-10-19 20:33:40 +0900

    added PayrollDatabase instead of DB

---
+++ src/main/java/jp/bacon/database/GlobalDatabase.java
@@ -0,0 +1,6 @@
+package jp.bacon.database;
+
+public class GlobalDatabase {
+
+  public static PayrollDatabase payrollDB;
+}
---
+++ src/main/java/jp/bacon/database/PayrollDatabase.java
@@ -0,0 +1,24 @@
+package jp.bacon.database;
+
+import java.util.List;
+
+import jp.bacon.domain.Employee;
+
+public interface PayrollDatabase {
+
+  void AddEmployee(int empId, Employee e);
+
+  Employee GetEmployee(int empId);
+
+  void Clear();
+
+  void DeleteEmployee(int empId);
+
+  void AddUnionMember(int memberId, Employee e);
+
+  Employee GetUnionMember(int memberId);
+
+  void RemoveUnionMember(int memberId);
+
+  List<Integer> GetAllEmployeeIds();
+}
