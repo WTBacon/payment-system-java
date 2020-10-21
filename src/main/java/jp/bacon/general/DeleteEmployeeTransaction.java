95b0a5adf7fb25692c96101bee70e632554b77c9
watabe-hiroaki <watabe_hiroaki@cyberagent.co.jp>
2020-10-21 23:39:36 +0900

    added DeleteEmployeeTransactin for deleting a employee

---
+++ src/main/java/jp/bacon/general/DeleteEmployeeTransaction.java
@@ -0,0 +1,17 @@
+package jp.bacon.general;
+
+import jp.bacon.database.GlobalDatabase;
+import jp.bacon.transaction.Transaction;
+
+public class DeleteEmployeeTransaction implements Transaction {
+
+  private final int itsEmpId;
+
+  public DeleteEmployeeTransaction(int empId) {
+    itsEmpId = empId;
+  }
+
+  public void Execute() {
+    GlobalDatabase.payrollDB.DeleteEmployee(itsEmpId);
+  }
+}
