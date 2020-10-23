d928a3bbf74299edb8694a9f71147623ffed1177
watabe-hiroaki <watabe_hiroaki@cyberagent.co.jp>
2020-10-23 23:30:53 +0900

    added TimeCard class

---
+++ src/main/java/jp/bacon/classifications/TimeCard.java
@@ -0,0 +1,21 @@
+package jp.bacon.classifications;
+
+import java.util.Calendar;
+
+public class TimeCard {
+	private final Calendar itsDate;
+	private final double itsHours;
+
+	public TimeCard(Calendar date, double hours) {
+		itsDate = date;
+		itsHours = hours;
+	}
+
+	public Calendar GetDate() {
+		return itsDate;
+	}
+
+	public double GetHours() {
+		return itsHours;
+	}
+}
